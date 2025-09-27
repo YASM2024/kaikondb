26da3133d760bb2768704191208ae0c598d39a7e
YASM2024 <ymiyazaki713@gmail.com>
2025-09-28 08:10:46 +0900

    ページネーションを読込みボタンに変更

--- resources/views/static/photos.blade.php
+++ resources/views/static/photos.blade.php
@@ -133,7 +133,7 @@
     </form>
     <div id="app" class="py-2 px-1 px-sm-4 mx-1 row"></div>
     <div id="number_of_show" class="m-1 px-1 px-sm-4"></div>
-    <div id="next_page_loader"></div>
+    <div id="next_page_loader" class="px-1 px-sm-4 mx-1 row"></div>
 </div>
 <div>
         <!-- モーダルの設定 -->
